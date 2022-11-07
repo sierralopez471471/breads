d85abf100f9761c937891b25f073404d830a492f
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-07 14:15:01 -0800

    delete route completed

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -26,6 +26,11 @@ breads.post('/', (req, res) => {
     res.redirect('/breads')
   })
   
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+    Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
+  })  
 
   // NEW
 breads.get('/new', (req, res) => {
@@ -36,7 +41,8 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
       res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
+        bread:Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex
       })
     } else {
       res.send('404')
