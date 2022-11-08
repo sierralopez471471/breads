69749214a4cde1dd98ddd2cd9492bc02c5274309
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-08 12:42:07 -0800

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -36,7 +36,26 @@ breads.delete('/:indexArray', (req, res) => {
 breads.get('/new', (req, res) => {
     res.render('new')
 })
- 
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray
+    })
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+      req.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+  })  
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
