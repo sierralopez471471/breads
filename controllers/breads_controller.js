616200163283d3987d00d58a739596b696e78cc6
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-13 10:20:44 -0800

    refactored for delete and update

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -30,10 +30,12 @@ breads.post('/', (req, res) => {
 })
   
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-    Bread.splice(req.params.indexArray, 1)
-    res.status(303).redirect('/breads')
-  })  
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
+})
 
   // NEW
 breads.get('/new', (req, res) => {
@@ -50,23 +52,28 @@ breads.get('/:id', (req, res) => {
 })
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-    res.render('edit', {
-      bread: Bread[req.params.indexArray],
-      index: req.params.indexArray
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
     })
 })
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
-    if(req.body.hasGluten === 'on'){
-      req.body.hasGluten = true
-    } else {
-      req.body.hasGluten = false
-    }
-    Bread[req.params.arrayIndex] = req.body
-    res.redirect(`/breads/${req.params.arrayIndex}`)
-  })  
+breads.put('/:id', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.redirect(`/breads/${req.params.id}`) 
+    })
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
