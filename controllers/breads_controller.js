bf99b55ea54236709ebb9df0b6f5ea216b61baea
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-13 11:32:27 -0800

    custom helper methods complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -37,18 +37,9 @@ breads.delete('/:id', (req, res) => {
     })
 })
 
-  // NEW
+// NEW
 breads.get('/new', (req, res) => {
     res.render('new')
-})
-
-breads.get('/:id', (req, res) => {
-  Bread.findById(req.params.id)
-      .then(foundBread => {
-          res.render('show', {
-              bread: foundBread
-          })
-      })
 })
 
 // EDIT
@@ -76,17 +67,16 @@ breads.put('/:id', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    if (Bread[req.params.arrayIndex]) {
-      res.render('Show', {
-        bread:Bread[req.params.arrayIndex],
-        index: req.params.arrayIndex
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+        const bakedBy = foundBread.getBakedBy() 
+        console.log(bakedBy)
+        res.render('show', {
+            bread: foundBread
+        })
       })
-    } else {
-      res.send('404')
-    }
-  })
-  
+    })
 
 module.exports = breads
 
