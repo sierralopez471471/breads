30255d85d49b094311a0639d0f644450d4882cdf
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-13 11:09:46 -0800

    implemented built-in validators

--- views/show.jsx
+++ views/show.jsx
@@ -16,6 +16,7 @@ function Show ({bread, index}) {
           have gluten.
         </p>
         <img src={bread.image} alt={bread.name} />
+        <p>Baked by {bread.baker}</p>
         <a href={`/breads/${bread.id}/edit`}>
             <button>
                 Edit
